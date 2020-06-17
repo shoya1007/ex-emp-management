169c38711d28039c916ec281261381ea45d86dd2
shoya1007 <shoya.fujisawa@rakus-partners.co.jp>
2020-06-17 13:55:57 +0900

    ログイン画面表示機能まで

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 /**
@@ -20,22 +21,41 @@ public class AdministratorController {
 	@Autowired
 	private AdministratorService administratorService;
 	
-	@ModelAttribute
+	
 	
 	/**
-	 * 
+	 * setUpAdministratorFormメソッド
 	 * @return インスタンス化したInsertAdministratorForm
-	 * リクエストパラメータを格納したオブジェクトをリクエストパラメータに格納するメソッド
+	 * リクエストパラメータを格納したオブジェクトをリクエストスコープに格納するメソッド
 	 */
+	@ModelAttribute
 	public InsertAdministratorForm setUpAdministratorForm() {
 		return new InsertAdministratorForm();
 	}
 	
+	/**
+	 * setUpLoginFormメソッド
+	 * @return インスタンス化したLoginFOrm
+	 * リクエストパラメータを格納したオブジェクトをリクエストスコープに格納するメソッド
+	 */
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		return new LoginForm();
+	}
+	/**
+	 * 
+	 * @return 入力画面にフォワード 
+	 */
 	@RequestMapping("/toInsert")
 	public String toInsert() {
 		return "administrator/insert";
 	}
 	
+	/**
+	 * 
+	 * @param form
+	 * @return 登録後の画面にリダイレクト
+	 */
 	@RequestMapping("/insert")
 	public String insert(InsertAdministratorForm form) {
 		Administrator administrator =new Administrator();
@@ -43,8 +63,19 @@ public class AdministratorController {
 		administrator.setMailAddress(form.getMailAddress());
 		administrator.setPassword(form.getPassword());
 		administratorService.insert(administrator);
-		return("/");
+		return("redirect:/");
 	}
+	
+	/**
+	 * 
+	 * @return ログイン画面にフォワード 
+	 */
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login";
+	}
+	
+	
 	
 	
 }
--- src/main/java/jp/co/sample/form/LoginForm.java
+++ src/main/java/jp/co/sample/form/LoginForm.java
@@ -1,5 +1,36 @@
 package jp.co.sample.form;
 
+/**
+ * ログイン時に使用するフォーム
+ * @author shoya
+ *
+ */
 public class LoginForm {
+	private String mailAddress;
+	
+	private String password;
 
+	public String getMailAddress() {
+		return mailAddress;
+	}
+
+	public void setMailAddress(String mailAddress) {
+		this.mailAddress = mailAddress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		// TODO 自動生成されたメソッド・スタブ
+		return super.toString();
+	}
+	
+	
 }
